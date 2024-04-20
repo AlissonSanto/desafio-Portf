425e85497a91b12f4da8ec2558fb41e61956dc36
Alisson Santo <alissonsantos6150@gmail.com.br>
2024-04-19 22:24:00 -0300

    adicionando o Projects e os Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Presentation from './Elementos/Componentes/Presentation/Presentation'
 import Header from './Elementos/Componentes/Header/Header'
+import Projects from './Elementos/Componentes/Projects/Projects'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Header/>
      <Presentation/>
+     <Projects/>
     </div>
   )
 }
