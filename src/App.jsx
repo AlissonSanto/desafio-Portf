a1b5b9f5102aa0f5b8966f4f831870d7f8b85170
Alisson Santo <alissonsantos6150@gmail.com.br>
2024-04-20 20:08:57 -0300

    Criando e Adicionando a TimeLine

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Presentation from './Elementos/Componentes/Presentation/Presentation'
-import Header from './Elementos/Componentes/Header/Header'
-import Projects from './Elementos/Componentes/Projects/Projects'
+import Presentation from './Elementos/Componentes/Presentation/Presentation';
+import Header from './Elementos/Componentes/Header/Header';
+import Projects from './Elementos/Componentes/Projects/Projects';
+import TimeLine from './Elementos/Componentes/TimeLine/TimeLine';
 import './App.css'
 
 function App() {
@@ -10,6 +11,29 @@ function App() {
       <Header/>
      <Presentation/>
      <Projects/>
+     <h1>Sobre Mim</h1> 
+        
+        <TimeLine year="2021"
+                  text="Me Formo no ensino medio pelo colegio
+                  Centro de Exelecia Atheneu Sergipense"
+                  />
+        <TimeLine year="2022"
+                  text="Inicio a Vida academica na faculdade,
+                        neste ano começei fazendo Educação fisica
+                        porem logo no segundo periodo tranquei o curso
+                        para seguir na area de tecnologia"
+                  />
+        <TimeLine year="2023"
+                  text="Começei a fazer um curso na Escola de Tecnologias DNC
+                        em FullStack. Não Satisfeito e a procura de 
+                        mais conhecimento dou inicio a varios mini-cursos
+                        de programação. "
+                  />
+        <TimeLine year="2024"
+                  text="Atras de um estagio ou emprego na area de desenvolvedor
+                        dou inicio no curso Ciencias da Computação 
+                        na Faculdade Estacio de Sá."
+                  />
     </div>
   )
 }
